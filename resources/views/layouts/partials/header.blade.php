8a2167905ccb9d2175124a9159a8e070ea71bfd3
Marcos Piñero <marcos.pinero1976@gmail.com>
2024-12-03 09:26:49 -0300

    varios

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,9 +1,10 @@
 <nav class="navbar navbar-light navbar-glass navbar-top navbar-expand">
 
     <button class="btn navbar-toggler-humburger-icon navbar-toggler me-1 me-sm-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarVerticalCollapse" aria-controls="navbarVerticalCollapse" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
-    <a class="navbar-brand me-1 me-sm-3" href="index.html">
-        <div class="d-flex align-items-center"><img class="me-2" src="assets/img/icons/spot-illustrations/falcon.png" alt="" width="40" /><span class="font-sans-serif text-primary">falcon</span>
+    <a class="navbar-brand me-1 me-sm-3" href="{{ route('home') }}">
+        <div class="d-flex align-items-center"><img class="me-2" src="{{ asset('images/logo_kaisen.png') }}" alt="" width="200" /><span class="font-sans-serif text-primary"></span>
         </div>
+
     </a>
     <!--<ul class="navbar-nav align-items-center d-none d-lg-block">
         <li class="nav-item">
