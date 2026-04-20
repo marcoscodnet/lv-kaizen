9f2e0d7bdcfa7fe251cd5772e46f956922fe66c5
Marcos Piñero <marcos.pinero1976@gmail.com>
2026-04-20 11:47:18 -0300

    varios

--- gulp/clean.gulp.js
+++ gulp/clean.gulp.js
@@ -7,7 +7,16 @@ const { paths, baseDir, version } = require('./utils');
 |  Clean
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 gulp.task('clean', () =>
-  del([`${baseDir}/${paths.style.dest}/**/*.*`, `${baseDir}/${paths.script.dest}/**/*.*`, `${baseDir}/**/*.html`])
+    del([
+        `${baseDir}/${paths.style.dest}/**/*.*`,
+        `${baseDir}/${paths.script.dest}/**/*.*`,
+        `!${baseDir}/${paths.script.dest}/combo-provincia-localidad.js`,
+        `!${baseDir}/${paths.script.dest}/combo-provincia-localidad-modal.js`,
+        `!${baseDir}/${paths.script.dest}/combo-sucursal-ubicacion.js`,
+        `!${baseDir}/${paths.script.dest}/confirm-exit.js`,
+        `!${baseDir}/${paths.script.dest}/flatpickr.js`,
+        `${baseDir}/**/*.html`
+    ])
 );
 gulp.task('clean:build', () => del(paths.dir.prod));
 gulp.task('clean:live', () => del(`live/${version}`));
